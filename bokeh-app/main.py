024bb7633aab0e8f494addc5b3d1b13de7c13e51
ZeteroDe <87331899+ZeteroDe@users.noreply.github.com>
2021-07-20 20:58:34 +0200

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -17,19 +17,17 @@ source = ColumnDataSource(data=dict(x=x, y=y, y2=y2, y3=y3))
 
 
 # Set up plot
-plot = figure(height=400, width=400, title="Reaction network",
+plot = figure(height=400, width=400, title="Reaction network. A -> B -> C",
               tools="crosshair,pan,reset,save,wheel_zoom",
               x_range=[0, 100], y_range=[0, 101])
-
-plot.line('x', 'y',  source=source, line_width=1, line_alpha=0.6)
-plot.line('x', 'y2', source=source, line_width=1, line_alpha=0.6)
-plot.line('x', 'y3', source=source, line_width=1, line_alpha=0.6)
-
-# Set up widgets
-
-fir = Slider(title="k1", value=0.1000000001, start=0.001, end=1, step=0.01)
+plot.line('x', 'y',  source=source, line_width=1, line_alpha=0.6, legend_label="A", line_color="green")
+plot.line('x', 'y2', source=source, line_width=1, line_alpha=0.6, legend_label="B", line_color="blue")
+plot.line('x', 'y3', source=source, line_width=1, line_alpha=0.6, legend_label="C", line_color="red")
+plot.xaxis.axis_label = 'Reaction / min'
+plot.yaxis.axis_label = 'Concentration / %'
+    # Set up widgets
+fir = Slider(title="k1", value=0.1, start=0.0010000000001, end=1, step=0.01)
 sec = Slider(title="k2", value=0.2, start=0.001, end=1, step=0.01)
-
 # Set up callbacks
 
 def update_data(attrname, old, new):
